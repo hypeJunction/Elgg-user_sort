2c93068cfd5269dc498fca571a6d778bd2dbf519
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-11 19:46:10 +0100

    fix(forms): fix how and when the sort form is displayed

--- views/default/forms/user/sort.php
+++ views/default/forms/user/sort.php
@@ -1,14 +1,4 @@
 <?php
-$sort_options = user_sort_get_sort_options();
-if (empty($sort_options)) {
-	return;
-}
-
-$sort_options_values = array();
-foreach ($sort_options as $sort_option) {
-	$sort_options_values[$sort_option] = elgg_echo("user:sort:$sort_option");
-}
-
 $fields = '';
 if (elgg_is_active_plugin('search') && elgg_extract('show_search', $vars, true)) {
 	$fields .= elgg_view_input('text', array(
@@ -21,14 +11,25 @@ if (elgg_is_active_plugin('search') && elgg_extract('show_search', $vars, true))
 	));
 }
 if (elgg_extract('show_sort', $vars, true)) {
-	$fields .= elgg_view_input('select', array(
-		'name' => 'sort',
-		'value' => elgg_extract('sort', $vars, 'time_created::desc'),
-		'options_values' => $sort_options_values,
-		'class' => 'user-sort-select',
-		'label' => elgg_echo('user:sort:label'),
-		'field_class' => 'user-sort-select-field',
-	));
+	$sort_options = user_sort_get_sort_options();
+	if (!empty($sort_options)) {
+		$sort_options_values = array();
+		foreach ($sort_options as $sort_option) {
+			$sort_options_values[$sort_option] = elgg_echo("user:sort:$sort_option");
+		}
+		$fields .= elgg_view_input('select', array(
+			'name' => 'sort',
+			'value' => elgg_extract('sort', $vars, 'time_created::desc'),
+			'options_values' => $sort_options_values,
+			'class' => 'user-sort-select',
+			'label' => elgg_echo('user:sort:label'),
+			'field_class' => 'user-sort-select-field',
+		));
+	}
+}
+
+if (!$fields) {
+	return;
 }
 
 echo elgg_format_element('div', [
